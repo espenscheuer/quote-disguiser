0e735f2a5f4e6d6d427cc5a5cdfe4ac6680c5955
Espen Scheuer <espenscheuer@gmail.com>
2020-02-15 10:40:38 -0800

    added python backend for test

--- src/App.js
+++ src/App.js
@@ -1,48 +1,35 @@
-import React, {useState} from 'react';
-import ContentEditable from "react-contenteditable";
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 
 function App() {
   const [text, setText] = useState("")
+  const [textContent, setTextContent] = useState('');
 
-  function handleChange(evt) {
-    var s = String(evt.target.value)
-    var a = s.split('><')
-    var a1 = a.map(str=>{
-      if(str.includes('>')) {
-        str = str.slice(str.indexOf('>') + 1, str.indexOf('</'))
+  async function updateText(e) {
+    await console.log(fetch(`/api`))
+	  setText(e.target.value);
+  };
+
+  useEffect(() => {
+    let generatedHTML = [];
+    text.split(' ').forEach((word, index) => {
+      const id = `word-${index}`;
+      let styles = {};
+      
+      if (word.startsWith('c')) {
+        styles['color'] = 'red';  
       }
-      var a2 = str.split(" ").map((str2, key) =>{
-        if(str2.includes("c")) {
-          var color = "red"
-        } else {
-          color = "black"
-        }
-        if(key > 0) {
-          str2 = " " + str2 
-        }
-        if(str2 !== " " && str2) {
-          return `<span style="color:${color};">${str2}</span>`
-        } else {
-          return ''
-        }
-      })
-      return a2.join('')
-    })
-    console.log((a1).join(''))
-    s = a1.join('')
+      generatedHTML.push(<><span key={id} style={styles}>{word}</span>{' '}</>);
+    });
+    setTextContent(generatedHTML);
+  }, [text]);
 
-    console.log("final = " + s)
-    setText(s)
-  }
-
-  return(
-  <ContentEditable className = "input"
-        html={text} // innerHTML of the editable div
-        disabled={false} // use true to disable edition
-        onChange={handleChange} // handle innerHTML change
-      />
+  return (
+    <div>
+      <textarea value={text} onChange={updateText}></textarea>
+      <div className="text-content">{textContent}</div>
+    </div>
   )
 }
 
