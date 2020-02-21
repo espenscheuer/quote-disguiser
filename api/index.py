8489ee02b5e486ff60993b461d952c1a719a8d7f
Espen Scheuer <espenscheuer@gmail.com>
2020-02-21 13:11:51 -0800

    fixed thing

--- api/index.py
+++ api/index.py
@@ -3,28 +3,27 @@ from bs4 import BeautifulSoup
 import urllib
 import requests
 def check(original, text):
-text = urllib.parse.quote(text)
-url = 'https://google.com/search?q={}'.format(text)
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-for g in soup.find_all(class_='ZINbbc xpd O9g5cc uUPGi'):
-  here = original in g.text
-  if here:
-    return(g.text)
-return ''
+	text = urllib.parse.quote(text)
+	url = 'https://google.com/search?q={}'.format(text)
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	for g in soup.find_all(class_='ZINbbc xpd O9g5cc uUPGi'):
+		here = original in g.text
+		if here:
+			return(g.text)
+	return ''
 class handler(BaseHTTPRequestHandler):
-def do_GET(self):
-  url_item = urllib.parse.urlparse(str(self.address_string))
-  params = urllib.parse.parse_qs(url_item.query)
-  if (params and 'original' in params and 'text' in params):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(check(params.original, params.text)).encode())
-  else:
-    self.send_response(400)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str('Please provide original and text').encode())
-  return
-
+	def do_GET(self):
+		url_item = urllib.parse.urlparse(self.path)
+		params = urllib.parse.parse_qs(url_item.query)
+		if (params and 'original' in params and 'text' in params):
+			self.send_response(200)
+			self.send_header('Content-type', 'text/plain')
+			self.end_headers()
+			self.wfile.write(str(check(params['original'][0], params['text'][0])).encode())
+		else:
+			self.send_response(400)
+			self.send_header('Content-type', 'text/plain')
+			self.end_headers()
+			self.wfile.write(str('Please provide original and text').encode())
+		return
