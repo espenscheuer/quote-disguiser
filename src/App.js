8497f854cd2843edd362742595fadc9ee06ff6ff
Espen Scheuer <espenscheuer@gmail.com>
2020-02-24 12:49:04 -0800

    added usability stuff

--- src/App.js
+++ src/App.js
@@ -1,146 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import Helmet from 'react-helmet';
-import { Button } from 'antd';
+import Home from './home.js'
+import About from './about.js'
+
+import { BrowserRouter, Route, Link} from 'react-router-dom'
 
 
 function App() {
-  const [text, setText] = useState('');
-  const [original, setOriginal] = useState('');
-  const [quote, setQuote] = useState(false)
-	const [found, setFound] = useState('');
-	const [textContent, setTextContent] = useState('');
-  const [btnText, setBtnText] = useState('Set Quote')
-
-	const onegram = require('./1_gram_json.json');
-
-	const updateText = e => {
-		setText(e.target.value);
-		if ('' === e.target.value) {
-			setOriginal('');
-			setFound('');
-		}
-	};
-
-	const apiRequest = async () => {
-		setOriginal(text);
-		setFound('');
-		const response = await fetch('/api/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				"original" : '"' + original + '"',
-				"text" : '"' + text + '"',
-			}),
-		});
-		response.text().then(value => {
-			if (value !== 'No results found') {
-				setFound(
-					<>
-						<blockquote>{(value + " on the first page")}</blockquote>
-						<iframe
-							src={`https://google.com/search?igu=1&q="${escape(text)}"`}
-							title="Search results"
-						/>
-					</>
-				);
-			} else {
-				setFound(
-					<>
-						<blockquote>{("Quote Found: " + value)}</blockquote>
-						<iframe
-							src={`https://google.com/search?igu=1&q="${escape(text)}"`}
-							title="Search results"
-						/>
-					</>
-				);
-			}
-		});
-  };
-  
-  useEffect(() => {
-    let generatedHTML = [];
-    let words = text.split(' ');
-    words.forEach((word, index) => {
-      const id = `word-${index}`;
-      let styles = {};
-      let included = original.includes(word)
-      let test = word.toLowerCase();
-      test = test.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ")
-      if (test in onegram && included) {
-        if(onegram[test] >= 10000) {
-          styles['color'] = 'gray';  
-        } else if(onegram[test] >= 5000) {
-          styles['color'] = '	#ff9a00';
-        } else if(onegram[test] >= 1000) {
-          styles['color'] = '#ff5a00'
-        } else {
-          styles['color'] = '#ff0000'
-        }
-      }
-      generatedHTML.push(<><span key={id} style={styles}>{word}</span>{' '}</>);
-    });
-    setTextContent(generatedHTML);
-  }, [text]);
 
 	return (
-		<div className="content">
-			<Helmet>
-				<html lang="en" />
-				<title>Differential Privacy Checker</title>
-				<meta name="robots" content="noindex, nofollow" />
-			</Helmet>
-      <h1>
-        Differential Privacy
-      </h1>
-      <p>
-        Enter the quote you would like to obfuscate.
-      </p>
-      
-			<div className="input">
-      {!quote && <textarea className="text-input" value={original} onChange={(e) => {setOriginal(e.target.value); setText(e.target.value)}} />}
-      {quote && <blockquote>{original}</blockquote>}
-        <button type = "primary"
-          onClick={(e) => {
-            setText(original) 
-            if(!quote) {
-              setBtnText("Edit Quote")
+    <BrowserRouter>
+        <Helmet>
+          <html lang="en" />
+          <title>Differential Privacy Checker</title>
+          <meta name="robots" content="noindex, nofollow" />
+        </Helmet>
+        <div className = 'content'>
+          <div className = "header">
+            <h1>
+              Differential Privacy
+            </h1>
+            <div className = "link-wrap">
               
-              e.target.style.backgroundColor = "lightGray"
-            } else {
-              setBtnText("Set Quote")
-              e.target.style.backgroundColor = '#1467ff'
-            }
-            
-            setQuote(!quote)}}>
-					{btnText}
-				</button>
-			</div>
-      {text && quote && (
-				<div>
-					<p>Now edit the quote, prioritizing the most unique words (in red) and check to see if the original quote appears in a google search.</p>
-					<textarea className="text-input" value={text} onChange={updateText} />
-				</div>
-			)}
-			{(textContent.length > 0 && text && quote) && (
-				<div>
-					<h3>Highlighted Quote</h3>
-          <p>Text highlighting is based on word uniqueness, something you can read more about in the info page.</p>
-					<blockquote>{textContent}</blockquote>
-          <button className="button" onClick={apiRequest}>
-					Check Quote
-				  </button>
-				</div>
-			)}
-			{found && quote && (
-				<div className="found">
-					<h3>Google Result</h3>
-					{found}
-				</div>
-			)}
-		</div>
+            </div>
+          </div>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+        </div>
+    </BrowserRouter>
 	);
 }
 
---
+++ src/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import { Link} from 'react-router-dom'
+
+
+function About(props) {
+    
+    return (
+        <div>
+            <div>
+                about text
+            </div>
+            <div>
+                <Link className = 'header-link' to ='/'> back to the tool </Link>
+            </div>
+        </div>
+    )
+}
+
+export default About
