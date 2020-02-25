25474cd4f450c09cd5d570b7df45807279deb261
Espen Scheuer <espenscheuer@gmail.com>
2020-02-25 12:23:23 -0800

    fixed garbage python

--- api/index.py
+++ api/index.py
@@ -16,35 +16,35 @@ def check(original, text):
 	return 'No results found'
 
 class handler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		url_item = urllib.parse.urlparse(self.path)
-		params = urllib.parse.parse_qs(url_item.query)
-		if (params and 'original' in params and 'text' in params):
-			self.send_response(200)
-			self.send_header('Content-type', 'text/plain')
-			self.end_headers()
-			self.wfile.write(str(check(params['original'][0], params['text'][0])).encode())
-		else:
-			self.send_response(400)
-			self.send_header('Content-type', 'text/plain')
-			self.end_headers()
-			self.wfile.write(str('Please provide original and text').encode())
-		return
+  def do_GET(self):
+    url_item = urllib.parse.urlparse(self.path)
+    params = urllib.parse.parse_qs(url_item.query)
+    if (params and 'original' in params and 'text' in params):
+      self.send_response(200)
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
+      self.wfile.write(str(check(params['original'][0], params['text'][0])).encode())
+    else:
+      self.send_response(400)
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
+      self.wfile.write(str('Please provide original and text').encode())
+    return
 
-	def do_POST(self):
-		content_length = int(self.headers.get('Content-Length'))
-		raw_data = self.rfile.read(content_length)
-		data = json.loads(raw_data.decode())
-		if (data and 'original' in data and 'text' in data):
+  def do_POST(self):
+    content_length = int(self.headers.get('Content-Length'))
+    raw_data = self.rfile.read(content_length)
+    data = json.loads(raw_data.decode())
+    if (data and 'original' in data and 'text' in data):
       print("recieved")
-			self.send_response(200)
-			self.send_header('Content-type', 'text/plain')
-			self.end_headers()
+      self.send_response(200)
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
       print(str(check(data['original'], data['text'])))
-			self.wfile.write(str(check(data['original'], data['text'])).encode())
-		else:
-			self.send_response(400)
-			self.send_header('Content-type', 'text/plain')
-			self.end_headers()
-			self.wfile.write(str('Please provide original and text').encode())
-		return
+      self.wfile.write(str(check(data['original'], data['text'])).encode())
+    else:
+      self.send_response(400)
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
+      self.wfile.write(str('Please provide original and text').encode())
+    return
