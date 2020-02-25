9756dd7dfa9e4338f1643406e34f3ee1794f0256
Espen Scheuer <espenscheuer@gmail.com>
2020-02-25 13:14:05 -0800

    revert p2

--- src/home.js
+++ src/home.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import { Link} from 'react-router-dom'
 
+
 function Home() {
     const [text, setText] = useState('');
     const [original, setOriginal] = useState('');
