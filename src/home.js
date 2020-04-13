34a11c50f348f3e127fd3a44ce532b8d0ba487b0
Espen Scheuer <espenscheuer@gmail.com>
2020-04-13 10:11:03 -0700

    hover for rarity added

--- src/home.js
+++ src/home.js
@@ -5,18 +5,19 @@ import {Link} from 'react-router-dom'
 import uniqid from 'uniqid';
 
 function Home() {
-    ReactGA.initialize('UA-162759087-1');
-    ReactGA.pageview('/home');
-    const [text, setText] = useState('');
-    const [original, setOriginal] = useState('');
-    const [quote, setQuote] = useState(false)
-    const [found, setFound] = useState('');
-    const [textContent, setTextContent] = useState('');
-    const [btnText, setBtnText] = useState('Set Quote')
-    const [google, setGoogle] = useState(false)
-    const [version, setVersion] = useState(0);
-    const [updates, setUpdates] = useState(null);
-    const [label, setLabel] = useState('');
+  ReactGA.initialize('UA-162759087-1');
+  ReactGA.pageview('/home');
+  const [text, setText] = useState('');
+  const [original, setOriginal] = useState('');
+  const [quote, setQuote] = useState(false)
+  const [found, setFound] = useState('');
+  const [textContent, setTextContent] = useState('');
+  const [btnText, setBtnText] = useState('Set Quote')
+  const [google, setGoogle] = useState(false)
+  const [version, setVersion] = useState(0);
+  const [updates, setUpdates] = useState(null);
+  const [label, setLabel] = useState('');
+  const [offset, setOffset] = useState({});
 
   const onegram = require('./1_gram_json.json');
   const twogram = require('./2_gram_json.json');
@@ -99,15 +100,18 @@ function Home() {
     uRed: 'u-red',
   };
 
-  const createLabel = (word, classNames) => {
+  const createLabel = (word, classNames, el) => {
+    setOffset({"top" : el.getBoundingClientRect().top,"left": el.getBoundingClientRect().left}) 
     if(classNames.includes(textClasses.gray)){
-      setLabel(word + ": common")
+      setLabel("common")
     } else if(classNames.includes(textClasses.orange)){
-      setLabel(word + ": uncommon")
+      setLabel("uncommon")
     } else if(classNames.includes(textClasses.darkOrange)){
-      setLabel(word + ": rare")
+      setLabel("rare")
     } else if(classNames.includes(textClasses.red)){
-      setLabel(word + ": very rare")
+      setLabel("very rare")
+    } else { 
+      setLabel("not in original")
     }
   };
 
@@ -118,8 +122,8 @@ function Home() {
     var n = s.length;
     var m = t.length;
 
-    if (n == 0) return m;
-    if (m == 0) return n;
+    if (n === 0) return m;
+    if (m === 0) return n;
 
     //Create an array of arrays in javascript (a descending loop is quicker)
     for (var i = n; i >= 0; i--) d[i] = [];
@@ -136,10 +140,10 @@ function Home() {
         for (var j = 1; j <= m; j++) {
 
             //Check the jagged ld total so far
-            if (i == j && d[i][j] > 4) return n;
+            if (i === j && d[i][j] > 4) return n;
 
             var t_j = t.charAt(j - 1);
-            var cost = (s_i == t_j) ? 0 : 1; // Step 5
+            var cost = (s_i === t_j) ? 0 : 1; // Step 5
 
             //Calculate the minimum
             var mi = d[i - 1][j] + 1;
@@ -152,7 +156,7 @@ function Home() {
             d[i][j] = mi; // Step 6
 
             //Damerau transposition
-            if (i > 1 && j > 1 && s_i == t.charAt(j - 2) && s.charAt(i - 2) == t_j) {
+            if (i > 1 && j > 1 && s_i === t.charAt(j - 2) && s.charAt(i - 2) === t_j) {
                 d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
             }
         }
@@ -213,7 +217,7 @@ function Home() {
       }
 
       const classNames = `${textColorClass} ${textUnderlineClass}`;
-      generatedHTML.push(<span key={id} onMouseEnter={() => {createLabel(word.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " "), classNames)}} className={classNames}>{word} </span>);
+      generatedHTML.push(<span key={id} onMouseEnter={(e)=> {createLabel(word, classNames, e.target)}} onMouseLeave={() => {setLabel('')}} className={classNames}>{word} </span>);
     });
     setVersion(version+1);
     setTextContent(generatedHTML);
@@ -271,7 +275,7 @@ function Home() {
         {quote && (
           <div>
             <p>Text highlighting is based on word uniqueness, something you can read more about in the info page.</p>
-            {label && <p>{label}</p>}
+            {label && <div className = "hover" style={{ "top" : offset.top - 35,"left": offset.left - 20}}>{label}</div>}
             <blockquote>{textContent}</blockquote>
             <button className="button" onClick={apiRequest}>
             Check Quote
